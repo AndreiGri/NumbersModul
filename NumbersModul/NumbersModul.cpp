9b0ef1f181285ab322b6375a0866c6eba9af7217
Andrei Gridin <andreigridin918@gmail.com>
2023-09-01 22:24:16 +0500

    Добавьте файлы проекта.

---
+++ NumbersModul/NumbersModul.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    // Изменение кодировки консоли:
+    system("chcp 1251 > nul");
+    int number = 0;
+    cout << "Введите число: ";
+    cin >> number;
+    int number1 = number;
+
+    if (number < 0) {
+        number1 = -number;
+    }
+
+    cout << "Число " << number << " по модулю равно " << number1 << endl;
+
+    // Задержка окна консоли:
+    system("pause > nul");
+    return 0;
+}
